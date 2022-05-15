08109c03d2c917eb10d1960e6e759366291ffb29
shahanneda <shahan.neda@gmail.com>
2022-05-15 12:56:39 -0700

    Documented innovation number

--- src/network/Gene.h
+++ src/network/Gene.h
@@ -3,10 +3,16 @@
 
 #define __GENE__
 
+/*
+Base class of network that stores the innovation number.
+*/
 class Gene
 {
 public:
 	Gene();
+	/*
+		A unique number assigned to a gene at the time of creation which tracks the historical origin of a gene and allows genomes to be compared with each other.
+	*/
 	const int innovationNumber;
 
 private:
