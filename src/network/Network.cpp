374edd9356c62ea5124fcfb0a23b232749ef2b3f
shahanneda <shahan.neda@gmail.com>
2022-05-19 23:25:00 -0700

    Worked on network

--- src/network/Network.cpp
+++ src/network/Network.cpp
@@ -1,9 +1,10 @@
 #include <algorithm>
+#include "Network.h"
 
 Network::Network(std::unordered_map<int, Connection> *connections, std::unordered_map<int, Neuron> *neurons)
 {
-	std::copy(connections->begin, connections->end, this->innovationConnectionsMap->begin());
-	std::copy(neurons->begin, neurons->end, this->innovationNeuronMap->begin());
+	// std::copy(connections->begin, connections->end, this->innovationConnectionsMap->begin());
+	// std::copy(neurons->begin, neurons->end, this->innovationNeuronMap->begin());
 }
 
 Network::~Network()
