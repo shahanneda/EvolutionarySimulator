2ea25f7fb790686a048ee45cca02240bf65a12da
shahanneda <shahan.neda@gmail.com>
2022-08-06 17:31:50 -0700

    Updated build script

--- publish_web.sh
+++ publish_web.sh
@@ -2,12 +2,12 @@
 git branch -D web
 git checkout -b web
 ./emc_full_build.sh
-cp build dist
+mv build dist
 cp web/index.html index.html
 cp web/coi-serviceworker.min.js coi-serviceworker.min.js
 rm .gitignore
 mv .gitignore-web .gitignore
 git add .
 # git commit -m "Built web"
-# git push
+# git push origin HEAD:web
 # git checkout master
